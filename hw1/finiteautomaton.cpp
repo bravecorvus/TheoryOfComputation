d785ef066fbb674f656ec2c8ec26c18c240f0d67
Andrew Lee <leeas@stolaf.edu>
2017-02-12 15:16:11 -0600

    partially complete

--- hw1/finiteautomaton.cpp
+++ hw1/finiteautomaton.cpp
@@ -1,6 +1,23 @@
+// FA for the language of all binary strings that have at least three symbols and whose first and last symbols are different
+#include <cstring>
 #include <iostream>
+#include <string>
+
 using namespace std;
 
+bool finiteautomaton(char *arg) {
+    cout << arg << endl;
+    return(true);
+}
+
 int main() {
-    
+    string userinput;
+    cout << "Add a binary string into the finite automaton. pick a sequnce made from two letters (alphanumerical) and press Return." << endl;
+    cin >> userinput;
+    char *cstringuserinput = new char[userinput.length() + 1];
+    strcpy(cstringuserinput, userinput.c_str());
+    // for (int i = 0; i < strlen(cstringuserinput); ++ i) {
+
+    // }
+    cout << finiteautomaton(cstringuserinput);
 }
