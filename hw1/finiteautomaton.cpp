67e4ee37312bfb663c852c28f7bc2f7e50df81ff
Andrew Lee <leeas@stolaf.edu>
2017-02-13 23:18:20 -0600

    hw1

--- hw1/finiteautomaton.cpp
+++ hw1/finiteautomaton.cpp
@@ -6,18 +6,39 @@
 using namespace std;
 
 bool finiteautomaton(char *arg) {
-    cout << arg << endl;
-    return(true);
+    if (strlen(arg) < 3) {
+        return(false);
+    }
+    char binaryfirst = arg[0];
+    char binarysecond;
+    for (int i = 0; i < strlen(arg); ++ i) {
+        if (arg[i] != binaryfirst) {
+            binarysecond = arg[i];
+            break;
+        }
+    } 
+    for (int i = 0; i < strlen(arg); ++i) {
+        if (arg[i] != binaryfirst && arg[i] != binarysecond) {
+            return(false);
+        }
+    }  
+    if (arg[0] != arg[strlen(arg)-1]) {
+        return(true);
+    } else {
+        return(false);
+    }
 }
 
 int main() {
     string userinput;
-    cout << "Add a binary string into the finite automaton. pick a sequnce made from two letters (alphanumerical) and press Return." << endl;
-    cin >> userinput;
-    char *cstringuserinput = new char[userinput.length() + 1];
-    strcpy(cstringuserinput, userinput.c_str());
-    // for (int i = 0; i < strlen(cstringuserinput); ++ i) {
-
-    // }
-    cout << finiteautomaton(cstringuserinput);
+    cout << "Add a binary string into the finite automaton. pick a sequnce made from two letters (alphanumerical) and press Return.\nEnter 'exit' then Return to exit program" << endl;
+    while(true) {
+        cin >> userinput;
+        if (userinput == "exit") {
+            break;
+        }
+        char *cstringuserinput = new char[userinput.length() + 1];
+        strcpy(cstringuserinput, userinput.c_str());
+        cout << boolalpha << finiteautomaton(cstringuserinput) << "\n" << endl;
+    }
 }
